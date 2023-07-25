6bcf9a115740f4439b0ed4f7d7dda1f5a051586b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-25 23:55:27 +0200

    Create toHex.c

---
+++ toHex.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "helper.h"
+/**
+ * toHex - converts to hex
+ * @num: number
+ * @base: eitehr 'a' or 'A'
+ *
+ * Return: Hex rep of num
+ */
+char *toHex(int num, char base)
+{
+	int length, i;
+	char *str;
+	int currentValue;
+
+	if (num == 0)
+		return ("0");
+	length = 8;
+	str = (char*)malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (str);
+	i = length - 1;
+	str[i--] = '\0';
+	while (num != 0)
+	{
+		currentValue = num % 16;
+		if (currentValue < 10)
+			str[i--] = '0' + (currentValue);
+		else
+			str[i--] = base + (currentValue - 10);
+		num /= 16;
+	}
+	return (&str[i + 1]);
+}
