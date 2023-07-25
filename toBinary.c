09184e511a1e1582de2421a714e2a3114da1491a
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-25 22:43:56 +0200

    Create toBinary.c

---
+++ toBinary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "helper.h"
+/**
+ * toBinary - converts unsigned int to binary
+ * @num: the number
+ *
+ * Return: returns string rep of num
+ */
+char *toBinary(unsigned int num)
+{
+	int length, i;
+	char *str;
+
+	if (num == 0)
+		return "0";
+	length = countDigits(num);
+	str = (char*)malloc(length * sizeof(char));
+	if (str == NULL)
+		return (str);
+	i = length - 1;
+	str[i--] = '\0';
+	while (num != 0)
+	{
+		str[i--] = '0' + (num & 1);
+		num >>= 1;
+	}
+	return (&str[i + 1]);
+}
