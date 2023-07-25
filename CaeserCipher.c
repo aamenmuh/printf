829640754bf932d4ed520e09d7d74cd483b998af
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-26 01:23:04 +0200

    Update CaeserCipher.c

--- CaeserCipher.c
+++ CaeserCipher.c
@@ -21,10 +21,10 @@ char *CaeserCipher(char *str, int shift)
 	i = 0;
 	for (i = 0; i < length; i++)
 	{
-		if (str[i] > 64 && str[i] < 91)
-			s[i] = (str[i] + shift) % 26 + 65;
-		else
-			s[i] = (str[i] + shift) % 26 + 97;
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			s[i] = (str[i] - 'A' + shift) % 26 + 'A';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			s[i] = (str[i] - 'a' + shift) % 26 + 'a';
 	}
 	return (s);
 }
