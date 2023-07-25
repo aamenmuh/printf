186681f6c1c3414e6e2d26bef7814a9fdce1a3fd
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-25 23:22:53 +0200

    Create unsignedToStr.c

---
+++ unsignedToStr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define MAX_UINT (4294967295)
+#include "helper.h"
+/**
+ * unsignedToStr - converts unsigned to str
+ * @num: the number
+ *
+ * Return: returns string rep
+ */
+char *unsignedToStr(unsigned int num){
+	int length;
+	int i;
+	char *str;
+	length = 0;
+
+	if (num > -1)
+		return (toString(num));
+	num = MAX_UINT + num + 1;
+	length = 11;
+	str = (char*)malloc(length * sizeof(char));
+	if (str == NULL)
+		return (str);
+	i = length - 1;
+	str[i--] = '\0';
+	while (num != 0)
+	{
+		str[i--] = '0' + (num % 10);
+		num /= 10;
+	}
+	return (&str[i + 1]);
+}
