6b3427cfef990b507ba500dc3ce482f9f67a95dc
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-29 03:15:39 +0300

    Update helper.h

--- helper.h
+++ helper.h
@@ -4,9 +4,10 @@ int countDigits(int num);
 char *toString(int num);
 char *toBinary(unsigned int num);
 char *unsignedToStr(unsigned int num);
-char *toAnyBase(long unsigned int num, int base, char c);
+char *toAnyBase(unsigned int num, int base, char c);
 int printConverted(char *str, int len);
 char *convertStringtoASCii(char *str);
 char *reverseString(char *str);
 char *CaeserCipher(char *str, int shift);
+char *toHex(long unsigned int num, char base);
 #endif
