f1283ca18c265e9c0f48626d293ce26e4b1a3d86
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-29 03:01:08 +0300

    Update helper.h

--- helper.h
+++ helper.h
@@ -4,7 +4,7 @@ int countDigits(int num);
 char *toString(int num);
 char *toBinary(unsigned int num);
 char *unsignedToStr(unsigned int num);
-char *toAnyBase(unsigned int num, int base, char c);
+char *toAnyBase(long unsigned int num, int base, char c);
 int printConverted(char *str, int len);
 char *convertStringtoASCii(char *str);
 char *reverseString(char *str);
